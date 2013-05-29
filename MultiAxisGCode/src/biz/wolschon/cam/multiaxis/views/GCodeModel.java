81702a068eaf5edf6eec5f41e3f1e0545d1325f6
Marcus Wolschon <marcus.wolschon@googlemail.com>
2013-05-29 18:23:32 +0200

    cleanup

--- MultiAxisGCode/src/biz/wolschon/cam/multiaxis/views/GCodeModel.java
+++ MultiAxisGCode/src/biz/wolschon/cam/multiaxis/views/GCodeModel.java
@@ -24,12 +24,30 @@ public class GCodeModel extends javax.swing.AbstractListModel {
 	 * Wraps a line of G-Code to display and the corresponding tool-location.
 	 */
 	public static class GCodeLine {
+		/**
+		 * The actual text of the line of G-Code.
+		 */
 		private String mLine;
+		/**
+		 * Location of every machine axis after performing this move.
+		 */
 		private double[] mToolLocation;
+		/**
+		 * The active tool during this step.
+		 */
 		private Tool mTool;
+		/**
+		 * 
+		 * @return The active tool during this step.
+		 */
 		public Tool getTool() {
 			return mTool;
 		}
+		/**
+		 * @param aLine The actual text of the line of G-Code.
+		 * @param aToolLocation Location of every machine axis after performing this move.
+		 * @param aTool The active tool during this step.
+		 */
 		public GCodeLine (final String aLine, final double[] aToolLocation, final Tool aTool) {
 			this.mLine = aLine;
 			this.mTool = aTool;
@@ -38,34 +56,58 @@ public class GCodeModel extends javax.swing.AbstractListModel {
 			}
 		}
 		/**
-		 * 
+		 * Return the location of every machine axis after performing this move.
 		 * @return May be null in header
 		 */
 		public double[] getToolLocation() {
 			return mToolLocation;
 		}
+		/**
+		 * @return The actual text of the line of G-Code.
+		 */
 		@Override
 		public String toString() {
 			return mLine;
 		}
 	}
+	/**
+	 * Our G-code lines to handle.
+	 */
 	private List<GCodeLine> mGCodeLines = new ArrayList<GCodeLine>();
+	/**
+	 * 
+	 * @param anIndex line number starting with 0
+	 * @return Location of every machine axis after performing the given move.
+	 */
 	public double[] getToolLocation(int anIndex) {
 		GCodeLine line = getElementAt(anIndex);
 		return line.getToolLocation();
 	}
+	/**
+	 * @param anIndex line number starting with 0
+	 * @return The {@link GCodeLine}.
+	 */
 	@Override
 	public GCodeLine getElementAt(int anIndex) {
 		return mGCodeLines.get(anIndex);
 	}
+	/**
+	 * @return the number of G-Code lines.
+	 */
 	@Override
 	public int getSize() {
 		return mGCodeLines.size();
 	}
+	/**
+	 * @param aGCodeLine a line of G-Code to add to the end of the program.
+	 */
 	public void addElement(final GCodeLine aGCodeLine) {
 		this.mGCodeLines.add(aGCodeLine);
 		fireIntervalAdded(this, getSize() - 1, getSize());
 	}
+	/**
+	 * Clear the program.
+	 */
 	public void clear() {
 		this.mGCodeLines.clear();
 		fireContentsChanged(this, 0, 0);		
